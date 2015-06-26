6c0449c7274ec200164f1a62039fd75bf6b70dbe
justinphelps <justin.phelps@gmail.com>
2015-06-26 16:19:08 +0200

    second commit

---
+++ lock_sequence.rb
@@ -0,0 +1,17 @@
+#20150626 Justin Phelps
+#Generates every combination sequence for a shift register based lock
+#This one transposes  numbers to odds for specific car door types
+#https://twitter.com/J0hnnyXm4s/status/613947889199501312/photo/1
+MAX = Integer 3124 #(5^5)-1
+(all_iterations ||= []) << "99999"
+(0..MAX).each do |x|
+  y = x.to_s(5)
+    y.tr!('4','9')
+    y.tr!('3','7')
+    y.tr!('2','5')
+    y.tr!('1','3')
+    y.tr!('0','1')
+     all_iterationsons << y.rjust(5,'1')
+end
+final_string = all_iterations.uniq.join + "99999"
+end
