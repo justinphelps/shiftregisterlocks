2085ad03c0d7c60595e9559c252df0c3eb438abd
Justin Phelps <justin.phelps@gmail.com>
2015-09-10 13:01:38 +0200

    Update lock_sequence.rb

--- lock_sequence.rb
+++ lock_sequence.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-#20150626 Justin Phelps
+#20150626 Justin Phelps ..
 #Generates every combination sequence for a shift register based lock
 #This one transposes base 5 numbers to base 10 odds for specific car door types
 #https://twitter.com/J0hnnyXm4s/status/613947889199501312/photo/1
